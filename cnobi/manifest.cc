8fd091958b51a2d5c2b9909d9754a12017a79d17
Pranav Bhatnagar <unknownblueguy6@gmail.com>
2024-11-27 01:26:24 -0500

    Update Binding type, simplify macros

--- cnobi/manifest.cc
+++ cnobi/manifest.cc
@@ -1,20 +1,17 @@
 #include "manifest.h"
 
-Rule compile = {
-  .name = "compile",
+RULE(compile)
   .command = START_EVAL L(g++) V(flags) L(-c) V(in) L(-o) V(out) END_EVAL,
-};
+END_RULE
 
-Rule link = {
-  .name = "link",
+RULE(link)
   .command = START_EVAL L(g++) V(in) L(-o) V(out) END_EVAL,
-};
+END_RULE
 
 Manifest manifest = {
-  .bindings =
-    START_BIND
-    B(flags, -O3)
-    END_BIND,
+  BINDINGS
+    BL(flags, "-O3")
+  END_BIND,
 
   .edges =
     START_EDGE
@@ -22,10 +19,10 @@ Manifest manifest = {
       .rule = &compile,
       .in = START_EVAL L(hello.cc) END_EVAL,
       .out = START_EVAL L(hello.o) END_EVAL,
-      .bindings =
-        START_BIND
-        B(flags, -O2)
-        END_BIND,
+      BINDINGS
+        BL(flags, "-O2")
+        BV(ldflags, "-L$builddir")
+      END_BIND,
     },
     {
       .rule = &link,
--- cnobi/manifest.h
+++ cnobi/manifest.h
@@ -1,16 +1,16 @@
 #ifndef CNOBI_MANIFEST_H
 #define CNOBI_MANIFEST_H
 
-struct Binding{
-  const char* val;
-  const char* var;
-};
-
-enum TokenType {LIT, VAR, END}; //RAW and SPECIAL
+enum Token {LIT, VAR, END}; //RAW and SPECIAL
 
 struct EvalString{
   const char* first;
-  const enum TokenType second;
+  const enum Token second;
+};
+
+struct Binding{
+  const char* val;
+  const struct EvalString* var;
 };
 
 struct RuleInfo{
@@ -50,27 +50,39 @@ struct EdgeInfo{
 
 struct StateInfo{
   const struct Binding* bindings;
-  const struct PoolInfo* pools;
   const struct EdgeInfo* edges;
   const struct EvalString* defaults;
+  const struct PoolInfo* pools;
+  const char** include;
+  const char** subninja;
 };
 
+#define LIT(X) {X, LIT},
+#define VAR(X) {X, VAR},
 #define L(X) {#X, LIT},
 #define V(X) {"$"#X, VAR},
 
-#define B(X, Y) {"$"#X, #Y},
+#define EVAL_NULL {"", END}
 
 #define START_EVAL (const struct EvalString[]){
-#define END_EVAL {"", END}}
+#define END_EVAL EVAL_NULL}
 
-#define START_BIND (const struct Binding[]){
-#define END_BIND {"", ""}}
+#define BINDINGS .bindings = (const struct Binding[]){
+#define END_BIND {"", START_EVAL END_EVAL}}
 
 #define START_EDGE (const struct EdgeInfo[]){
 #define END_EDGE {.rule=0}}
 
+#define BL(X, Y) {#X, START_EVAL LIT(Y) END_EVAL},
+#define BV(X, Y) {#X, START_EVAL VAR(Y) END_EVAL},
+
 #define Rule const struct RuleInfo
 #define Pool const struct PoolInfo
 #define Manifest const struct StateInfo
+
+#define RULE(X) Rule X = { \
+.name = #X,
+
+#define END_RULE };
 
 #endif
